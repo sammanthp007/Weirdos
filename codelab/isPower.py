d79582a4a899c7eb4c2d1bade54b2491b75f56f0
sammanthp007 <samman.thapa@bison.howard.edu>
2016-11-24 08:15:34 -0500

    implement codelab/isPower.py

--- codelab/isPower.py
+++ codelab/isPower.py
@@ -8,12 +8,14 @@ class Solution:
         if N == 1:
             return True
         for p in range(2, 33):
-            for A in range(2, int(N**(1.0 / p)) + 2):
-                if A**p == N:
-                    print(A, p)
+            coef = int(N**(1.0 / p))
+            for i in range(2, coef + 2):
+                if i**p == N:
+                    print(coef, p)
                     return True
         return False
 
-
+num = 823543
+print(num ** (1.0 / 7))
 a = Solution()
 print(a.isPower(823543))
