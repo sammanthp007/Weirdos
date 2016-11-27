1081c941becff39deeee0aeae39ae8d3c6e5843e
sammanthp007 <samman.thapa@bison.howard.edu>
2016-11-27 13:28:12 -0500

    improve time complexity of codelab/minim...py

--- codelab/minimum number of steps to reach another coordinate.py
+++ codelab/minimum number of steps to reach another coordinate.py
@@ -16,24 +16,7 @@ class Solution:
         return total_steps
 
     def min_num_steps_to_reach(self, c_x, c_y, f_x, f_y):
-        min_num_steps = 0
-        if self.same_point(c_x, c_y, f_x, f_y):
-            return 0
-        if self.within_range(c_x, c_y, f_x, f_y):
-            return 1
-        if c_x == f_x:
-            return abs(c_y - f_y)
-        if c_y == f_y:
-            return abs(c_x - f_x)
-        if c_x > f_x:
-            c_x -= 1
-        else:
-            c_x += 1
-        if c_y > f_y:
-            c_y -= 1
-        else:
-            c_y += 1
-        return 1 + self.min_num_steps_to_reach(c_x, c_y, f_x, f_y)
+        return max(abs(c_x - f_x), abs(c_y - f_y))
 
     def same_point(self, x, y, x1, y1):
         return (x == x1 and y == y1)
@@ -46,24 +29,3 @@ ys = [0, 2, -28]
 a = Solution()
 s = a.coverPoints(xs, ys)
 print(s)
-
-
-class Solution:
-        # @param X : list of integers
-            # @param Y : list of integers
-                # Points are represented by (X[i], Y[i])
-                    # @return an integer
-
-    def coverPoints(self, X, Y):
-        ret = 0
-        n = len(X)
-        if n == 0:
-            return ret
-        curr = (X[0], Y[0])
-
-        for i in range(1, n):
-            stepsX = abs(curr[0] - X[i])
-            stepsY = abs(curr[1] - Y[i])
-            ret += max(stepsX, stepsY)
-            curr = (X[i], Y[i])
-        return ret
