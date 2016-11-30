13511a3a8f737d9f9d625b71a82c5be755cfc99a
Samman Bikram Thapa <samman.thapa@bison.howard.edu>
2016-11-29 19:53:40 -0500

    update proving_ground.py

--- proving_ground.py
+++ proving_ground.py
@@ -1,19 +1,1 @@
-class Solution:
-        # @param A : integer
-            # @return a boolean
 
-    def isPower(self, N):
-        if N == 0:
-            return False
-        if N == 1:
-            return True
-        for p in range(2, 33):
-            for A in range(2, int(N**(1.0 / p)) + 2):
-                if A**p == N:
-                    print(A, p)
-                    return True
-        return False
-
-
-a = Solution()
-print(a.isPower(823543))
